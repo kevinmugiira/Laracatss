6aa8b980a67ad289742db5e4d608b411c6179687
kevinmugiira <kevinm5740@aol.com>
2020-09-29 15:18:32 +0300

    applied navigation bar selection to all the nav bar items

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,30 +5,27 @@
 @section('workspace-content')
 
 
+    @section('header')
     <div id="header-wrapper">
         <div id="header" class="container">
             <div id="logo">
-                <h1><a href="/">SimpleWork</a></h1>
+                <h1><a href="#">SimpleWork</a></h1>
             </div>
             <div id="menu">
                 <ul>
                     <li class="{{Request::path() === '/' ? 'current_page_item':''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
                     <li class="{{Request::path() === 'clients' ? 'current_page_item':''}}"><a href="#" accesskey="2" title="">Our Clients</a></li>
-                    <li class="{{Request::path() === 'about' ? 'current_page_item':''}}"v><a href="/about" accesskey="3" title="">About Us</a></li>
+                    <li class="{{Request::path() === 'about' ? 'current_page_item':''}}"><a href="/about" accesskey="3" title="">About Us</a></li>
                     <li class="{{Request::path() === 'careers' ? 'current_page_item':''}}"><a href="#" accesskey="4" title="">Careers</a></li>
                     <li class="{{Request::path() === 'contact' ? 'current_page_item':''}}"><a href="/contact" accesskey="5" title="">Contact Us</a></li>
                 </ul>
             </div>
         </div>
-        <div id="header-featured">
-            <div id="banner-wrapper">
-                <div id="banner" class="container">
-                    <h2>Maecenas luctus lectus</h2>
-                    <p>This is <strong>SimpleWork</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
-                    <a href="#" class="button">Etiam posuere</a> </div>
-            </div>
-        </div>
+
     </div>
+    @endsection
+
+
     <div id="wrapper">
         <div id="page" class="container">
             <div id="content">
@@ -83,4 +80,4 @@
     </div>
 
 
-    @endsection
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});

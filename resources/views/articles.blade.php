1a7d7ebdb559191f9b735e826b445b6e0314dd2a
kevinmugiira <kevinm5740@aol.com>
2020-10-15 23:22:52 +0300

    added articles view

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -16,7 +16,7 @@
                     <li class="{{Request::path() === '/' ? 'current_page_item':''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
                     <li class="{{Request::path() === 'clients' ? 'current_page_item':''}}"><a href="#" accesskey="2" title="">Our Clients</a></li>
                     <li class="{{Request::path() === 'about' ? 'current_page_item':''}}"><a href="/about" accesskey="3" title="">About Us</a></li>
-                    <li class="{{Request::path() === 'Articles' ? 'current_page_item':''}}"><a href="#" accesskey="4" title="">Articles</a></li>
+                    <li class="{{Request::path() === 'Articles' ? 'current_page_item':''}}"><a href="/articles" accesskey="4" title="">Articles</a></li>
                     <li class="{{Request::path() === 'contact' ? 'current_page_item':''}}"><a href="/contact" accesskey="5" title="">Contact Us</a></li>
                 </ul>
             </div>
@@ -33,8 +33,8 @@
                 <h2>Welcome to our articles page</h2>
                 <span class="byline">Mauris vulputate dolor sit amet nibh</span> </div>
 
-        </div>
-        <div id="sidebar">
+
+        <!--<div id="sidebar">-->
 
             @foreach($articl as $articles)
                 <ul class="style1">
@@ -42,12 +42,14 @@
                         <h3>
                             <a href="articles/{{ $articles->id }}"> {{$articles->title}}</a>
                         </h3>
+
+                        <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
+
                         <p>{{$articles->excerpt}}</p>
                     </li>
                 </ul>
             @endforeach
-
-
+        </div>
         </div>
     </div>
 </div>
